a6bdb8e1e7d67863c7d423962aeea574e3aa72b9
Dennis Durling <djdtahoe@gmail.com>
2025-10-08 15:01:29 -0700

    super simple nvidia top daemon, not efficient or anything, just learning rust...

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,49 @@
 use nvml_wrapper::Nvml;
+use std::io::{self, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
-fn main() -> Result<(), nvml_wrapper::error::NvmlError> {
-    let nvml = Nvml::init()?;
+fn main() -> io::Result<()> {
+    loop {
+        println!("\x1B[2J\x1B[1;1H");
+        print_nvl();
+        io::stdout().flush()?;
+        sleep(Duration::from_secs(1));
+    }
+}
+
+fn print_nvl() {
+    let nvml = Nvml::init();
     // Get the first `Device` (GPU) in the system
-    let device = nvml.device_by_index(0)?;
+    let nvml_obj = nvml.expect("wtf"); //.device_by_index(0).unwrap();
+    let device = nvml_obj.device_by_index(0).unwrap();
 
-    let brand = device.brand()?; // GeForce on my system
+    // Brand is simple...
+    let brand = device.brand().unwrap(); // GeForce on my system
     println!("Brand: {:?}", brand);
-    let fan_speed = device.fan_speed(0)?; // Currently 17% on my system
-    println!("Fan Speed: {fan_speed}");
-    let power_limit = device.enforced_power_limit()?; // 275k milliwatts on my system
-    println!("Power Limit: {power_limit}");
-    let encoder_util = device.encoder_utilization()?; // Currently 0 on my system; Not encoding anything
-    println!("Encoder Use: {:?}", encoder_util);
-    let gpu_util = device.utilization_rates()?;
-    println!("GPU Use: {:?}", gpu_util);
-    let memory_info = device.memory_info()?; // Currently 1.63/6.37 GB used on my system
-                                             // println!("Memory Info: {:?}", memory_info);
-    let mem_used = memory_info.used / 1_000_000_000;
-    let mem_total = memory_info.total / 1_000_000_000;
-    println!("Memory Usage: Used:{mem_used}GB, Total:{mem_total}");
+
+    // Fan speed is also simple
+    let fan_speed = device.fan_speed(0).unwrap(); // Currently 17% on my system
+    println!("Fan Speed: {:?}%", fan_speed);
+
+    // Power output is simple but we want in Watts, not micro watts...
+    let pwr_wtts_used = device.power_usage().unwrap() / 1000;
+    let pwr_wtts_cap = device.power_management_limit().unwrap() / 1000;
+    println!("Power Usage: Used:{pwr_wtts_used}W, Max:{pwr_wtts_cap}W");
+
+    // Get base outputs from device
+    let memory_info = device.memory_info().unwrap();
+    let gpu_util = device.utilization_rates().unwrap().gpu;
+    // let encoder_util = device.encoder_utilization();
+    // let decoder_util = device.decoder_utilization();
+
+    // Get MEM specific outputs and print
+    let mem_used = format!("{:.2}", memory_info.used as f32 / 1_000_000_000.0);
+    let mem_total = format!("{:.2}", memory_info.total as f32 / 1_000_000_000.0);
+    println!("Memory Usage: Used:{mem_used}GB, Total:{mem_total}GB");
+
+    // get GPU specific usage info and print
+    println!("GPU Use: {:?}%", gpu_util);
     // println!("Memory Info: {:?}", memory_info);
     //
     // free: 11459231744,
@@ -30,5 +54,6 @@ fn main() -> Result<(), nvml_wrapper::error::NvmlError> {
     //
 
     // ... and there's a whole lot more you can do. Most everything in NVML is wrapped and ready to go
-    Ok(())
+    //let _ = io::stdout().flush();
+    //Ok(())
 }
