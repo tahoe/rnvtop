51abc0b1bf0ce91308972d91daae4034b196756c
Dennis Durling <djdtahoe@gmail.com>
2025-10-17 14:44:54 -0700

    Updated stats struct to break it out into multiple structs. This semi broke the table view, though it is more compact, it is not pretty... This made the json view much nicer.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use chrono::offset::Local as localtime;
 use clap::Parser;
+use colored_json::to_colored_json_auto;
 use nvml_wrapper::{Device, Nvml};
 use owo_colors::OwoColorize;
 use owo_colors::Stream::Stdout;
 use serde::Serialize;
-
-use colored_json::to_colored_json_auto;
+use std::fmt;
 use std::io;
 use std::thread::sleep;
 use std::time::Duration;
 use tabled::{
-    settings::{object::Rows, Color, Style},
+    settings::{object::Rows, Color, Concat, Style},
     Table, Tabled,
 };
 
@@ -103,25 +103,77 @@ struct Args {
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
-pub struct Stats {
-    #[tabled(rename = "Fan")]
-    pub fan_speed: u32,
-    #[tabled(rename = "Temp")]
-    pub gpu_temp: u32,
-    #[tabled(rename = "PWR Used")]
-    pub pwr_used: u32,
-    #[tabled(rename = "PWR Max")]
-    pub pwr_cap: u32,
+pub struct GpuStats {
     #[tabled(rename = "GPU Util")]
     pub gpu_util: u32,
     #[tabled(rename = "Enc Util")]
     pub enc_util: u32,
     #[tabled(rename = "Dec Util")]
     pub dec_util: u32,
-    #[tabled(rename = "Mem Used")]
+}
+
+impl fmt::Display for GpuStats {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "gpu_util: {}, enc_util: {}, dec_util: {}",
+            self.gpu_util, self.enc_util, self.dec_util
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
+pub struct FanTemp {
+    #[tabled(rename = "Fan Speed")]
+    pub fan_speed: u32,
+    #[tabled(rename = "GPU Temp")]
+    pub gpu_temp: u32,
+}
+
+impl fmt::Display for FanTemp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "fan_speed: {}, gpu_temp: {}",
+            self.fan_speed, self.gpu_temp
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
+pub struct Power {
+    #[tabled(rename = "PWR Used")]
+    pub pwr_used: u32,
+    #[tabled(rename = "PWR Max")]
+    pub pwr_cap: u32,
+}
+
+impl fmt::Display for Power {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "pwr_used: {}, pwr_cap: {}", self.pwr_used, self.pwr_cap)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
+pub struct Memory {
+    #[tabled(rename = "Memory Used")]
     pub mem_used: f32,
-    #[tabled(rename = "Mem Total")]
+    #[tabled(rename = "Memory Total")]
     pub mem_total: f32,
+}
+
+impl fmt::Display for Memory {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "mem_used: {}, mem_total: {}",
+            self.mem_used, self.mem_total
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
+pub struct DeviceInfo {
     #[tabled(rename = "Driver Ver")]
     pub drvr_ver: String,
     #[tabled(rename = "Cuda Ver")]
@@ -130,56 +182,115 @@ pub struct Stats {
     pub dev_name: String,
 }
 
+impl fmt::Display for DeviceInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "drvr_ver: {}, cuda_ver: {}, dev_name: {}",
+            self.drvr_ver, self.cuda_ver, self.dev_name
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Tabled)]
+pub struct Stats {
+    // DeviceInfo sub struct
+    #[tabled(rename = "DeviceInfo")]
+    pub devinfo: DeviceInfo,
+
+    // Memory sub struct
+    #[tabled(rename = "Memory")]
+    pub memory: Memory,
+
+    // GPU Stats sub struct
+    #[tabled(rename = "GPU Stats")]
+    pub gpustats: GpuStats,
+
+    // Temp and Fan speed sub struct
+    #[tabled(rename = "Fan/Temp")]
+    pub fantemp: FanTemp,
+
+    // Power sub struct
+    #[tabled(rename = "Power")]
+    pub power: Power,
+}
+
 impl Stats {
     pub fn new(device: &Device) -> Self {
-        let dev_name = device.name().unwrap_or("unknown".into());
+        // get fan/temp info
         let fan_speed = device.fan_speed(0).unwrap_or(0); // Currently 17% on my system
         let gpu_temp = device
             .temperature(nvml_wrapper::enum_wrappers::device::TemperatureSensor::Gpu)
             .unwrap_or(0);
+
+        // populate FanTemp struct
+        let fantemp = FanTemp {
+            fan_speed,
+            gpu_temp,
+        };
+
+        // get power info
         let pwr_used = device.power_usage().unwrap_or(0) / 1000;
         let pwr_cap = device.power_management_limit_default().unwrap_or(0) / 1000;
 
+        // populate Power struct
+        let power = Power { pwr_used, pwr_cap };
+
+        // get utilization info
         let gpu_util = device.utilization_rates().unwrap().gpu;
         let enc_util = device.encoder_utilization().unwrap().utilization;
         let dec_util = device.decoder_utilization().unwrap().utilization;
+
+        // populate GpuStats
+        let gpustats = GpuStats {
+            gpu_util,
+            enc_util,
+            dec_util,
+        };
 
         // Get MEM specific outputs and print
         let memory_info = device.memory_info().unwrap();
         let mem_used = memory_info.used as f32 / 1_024_000_000.0;
         let mem_total = memory_info.total as f32 / 1_024_000_000.0;
 
+        // populate Memory struct
+        let memory = Memory {
+            mem_used,
+            mem_total,
+        };
+
         // get the driver memory_info
+        let dev_name = device.name().unwrap_or("unknown".into());
         let drvr_ver = device
             .nvml()
             .sys_driver_version()
             .unwrap_or("N/A".to_owned());
-
-        // get the driver memory_info
         let cuda_ver = device.nvml().sys_cuda_driver_version().unwrap_or(0) as f32 / 1000.0;
-        Self {
-            fan_speed,
-            gpu_temp,
-            pwr_cap,
-            pwr_used,
-            gpu_util,
-            enc_util,
-            dec_util,
-            mem_total,
-            mem_used,
+
+        // populate the DeviceInfo struct
+        let devinfo = DeviceInfo {
             drvr_ver,
             cuda_ver,
             dev_name,
+        };
+
+        Self {
+            devinfo,
+            memory,
+            power,
+            fantemp,
+            gpustats,
         }
     }
 }
 
 fn print_oneline(device: &Device) {
-    let stats = Stats::new(device);
-    println!(
-        "{:?} Enc: {:?} Dec: {:?} Tmp: {:?} Fan: {:?}\r",
-        stats.gpu_util, stats.enc_util, stats.dec_util, stats.gpu_temp, stats.fan_speed
-    );
+    let _stats = Stats::new(device);
+    println!("Oneliner");
+    // println!(
+    //     "{:?} Enc: {:?} Dec: {:?} Tmp: {:?} Fan: {:?}\r",
+    //     stats.gpu_util, stats.enc_util, stats.dec_util, stats.gpu_temp, stats.fan_speed
+    // );
 }
 
 fn print_json(device: &Device) {
@@ -189,15 +300,51 @@ fn print_json(device: &Device) {
 }
 
 fn print_tabular(device: &Device, colorize: bool) {
+    //     devinfo,
+    //     memory,
+    //     power,
+    //     fantemp,
+    //     gpustats,
     let stats = Stats::new(device);
-    let v_stats = vec![stats];
-    let mut table = Table::new(v_stats);
-    table.with(Style::rounded());
+    let v_devinfo = vec![stats.devinfo];
+    let v_memory = vec![stats.memory];
+    let v_power = vec![stats.power];
+    let v_fantemp = vec![stats.fantemp];
+    let v_gpustats = vec![stats.gpustats];
+
+    let mut devinfo_table = Table::new(v_devinfo);
+    let mut memory_table = Table::new(v_memory);
+    let mut power_table = Table::new(v_power);
+    let mut fantemp_table = Table::new(v_fantemp);
+    let mut gpustats_table = Table::new(v_gpustats);
+
+    devinfo_table.with(Style::rounded());
+    memory_table.with(Style::rounded());
+    power_table.with(Style::rounded());
+    fantemp_table.with(Style::rounded());
+    gpustats_table.with(Style::rounded());
+
     if colorize {
-        table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
-        table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
+        devinfo_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+        devinfo_table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
+
+        memory_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+        memory_table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
+
+        power_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+        power_table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
+
+        fantemp_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+        fantemp_table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
+
+        gpustats_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+        gpustats_table.modify(Rows::last(), Color::FG_BRIGHT_CYAN);
     }
-    println!("{}", table);
+    devinfo_table.with(Concat::vertical(memory_table));
+    devinfo_table.with(Concat::vertical(power_table));
+    devinfo_table.with(Concat::vertical(fantemp_table));
+    devinfo_table.with(Concat::vertical(gpustats_table));
+    println!("{}", devinfo_table);
 }
 
 fn print_multiliner(device: &Device, looping: bool, colorize: bool) {
@@ -220,30 +367,45 @@ fn print_multiliner(device: &Device, looping: bool, colorize: bool) {
     println!(
         "{} {}\r",
         "GPU:".if_supports_color(Stdout, |gpu| gpu.red()),
-        stats.dev_name.if_supports_color(Stdout, |name| name.cyan())
+        stats
+            .devinfo
+            .dev_name
+            .if_supports_color(Stdout, |name| name.cyan())
     );
 
     // print the driver info
     println!(
         "{} {} {} {:.1}\r",
         "Driver Ver:".if_supports_color(Stdout, |ver| ver.red()),
-        stats.drvr_ver.if_supports_color(Stdout, |drvr| drvr.cyan()),
+        stats
+            .devinfo
+            .drvr_ver
+            .if_supports_color(Stdout, |drvr| drvr.cyan()),
         "CUDA Ver:".if_supports_color(Stdout, |ver| ver.red()),
-        stats.cuda_ver.if_supports_color(Stdout, |drvr| drvr.cyan()),
+        stats
+            .devinfo
+            .cuda_ver
+            .if_supports_color(Stdout, |drvr| drvr.cyan()),
     );
 
     // print the fan speed
     println!(
         "{} {:?}\r",
         "Fan Speed: ".if_supports_color(Stdout, |clr| clr.red()),
-        stats.fan_speed.if_supports_color(Stdout, |clr| clr.cyan())
+        stats
+            .fantemp
+            .fan_speed
+            .if_supports_color(Stdout, |clr| clr.cyan())
     );
 
     // print the gpu temp
     println!(
         "{} {:?}{}\r",
         "GPU Temp:".if_supports_color(Stdout, |clr| clr.red()),
-        stats.gpu_temp.if_supports_color(Stdout, |clr| clr.cyan()),
+        stats
+            .fantemp
+            .gpu_temp
+            .if_supports_color(Stdout, |clr| clr.cyan()),
         "c".yellow()
     );
 
@@ -252,9 +414,15 @@ fn print_multiliner(device: &Device, looping: bool, colorize: bool) {
         "{} {}{:?}, {}{:?}\r",
         "Power Usageg:".if_supports_color(Stdout, |clr| clr.red()),
         "Used:".if_supports_color(Stdout, |clr| clr.cyan()),
-        stats.pwr_used.if_supports_color(Stdout, |clr| clr.yellow()),
+        stats
+            .power
+            .pwr_used
+            .if_supports_color(Stdout, |clr| clr.yellow()),
         "Max:".if_supports_color(Stdout, |clr| clr.cyan()),
-        stats.pwr_cap.if_supports_color(Stdout, |clr| clr.yellow()),
+        stats
+            .power
+            .pwr_cap
+            .if_supports_color(Stdout, |clr| clr.yellow()),
     );
 
     // print the mem used/total
@@ -262,9 +430,13 @@ fn print_multiliner(device: &Device, looping: bool, colorize: bool) {
         "{} {}{:.2?}, {}{:.2?}\r",
         "Memory Usage:".if_supports_color(Stdout, |clr| clr.red()),
         "Used:".if_supports_color(Stdout, |clr| clr.cyan()),
-        stats.mem_used.if_supports_color(Stdout, |clr| clr.yellow()),
+        stats
+            .memory
+            .mem_used
+            .if_supports_color(Stdout, |clr| clr.yellow()),
         "Max:".if_supports_color(Stdout, |clr| clr.cyan()),
         stats
+            .memory
             .mem_total
             .if_supports_color(Stdout, |clr| clr.yellow()),
     );
@@ -274,10 +446,19 @@ fn print_multiliner(device: &Device, looping: bool, colorize: bool) {
     println!(
         "{} {:?}% {} {:?}% {} {:?}%\r",
         "GPU Usage:".if_supports_color(Stdout, |clr| clr.red()),
-        stats.gpu_util.if_supports_color(Stdout, |clr| clr.cyan()),
+        stats
+            .gpustats
+            .gpu_util
+            .if_supports_color(Stdout, |clr| clr.cyan()),
         "Encoder:".if_supports_color(Stdout, |clr| clr.red()),
-        stats.enc_util.if_supports_color(Stdout, |clr| clr.cyan()),
+        stats
+            .gpustats
+            .enc_util
+            .if_supports_color(Stdout, |clr| clr.cyan()),
         "Decoder:".if_supports_color(Stdout, |clr| clr.red()),
-        stats.dec_util.if_supports_color(Stdout, |clr| clr.cyan())
+        stats
+            .gpustats
+            .dec_util
+            .if_supports_color(Stdout, |clr| clr.cyan())
     );
 }
